0a1de70d83c1fa9a6248e00fa2881eb75cea14cc
srinjoy pati <91176055+srinjoy-26@users.noreply.github.com>
2025-07-07 15:21:02 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,14 +7,25 @@ document.getElementById('contactForm').addEventListener('submit', (e)=>{
     e.preventDefault();
     console.log(Nameval.value , Emailval.value , Inq_cat.value , Messageval.value);
     charactersInMessage = Messageval.value.length;
-    gtag('event', 'contact_form_submit', {
-                    inquiry_category: Inq_cat.value, 
-                    characters_in_message: charactersInMessage,
-                    submission_count: 1,
-                    form_id: 'contactForm',
-                    form_name: 'Contact Us Form', 
-                    page_location: window.location.href
-                })
-    console.log('GA4: add_to_cart event sent for:', Nameval.value )
+    // gtag('event', 'contact_form_submit', {
+    //                 inquiry_category: Inq_cat.value, 
+    //                 characters_in_message: charactersInMessage,
+    //                 submission_count: 1,
+    //                 form_id: 'contactForm',
+    //                 form_name: 'Contact Us Form', 
+    //                 page_location: window.location.href
+    //             })
+    digitalData.contactform.inquiry_category: Inq_cat.value,
+    digitalData.contactform.characters_in_message: charactersInMessage,
+    digitalData.contactform.inquiry_category: Inq_cat.value,
+    digitalData.contactform.submission_count: 1,
+    digitalData.contactform.form_name: 'Contact Us Form',
+    digitalData.contactform.page_location: window.location.href
+    digitalData.event.push({
+                        name: "contactformsubmitted",
+                        user: Nameval.value,
+                        message: Messageval.value
+                 });
+    console.log('contact form submitted', digitalData)
 
 })
